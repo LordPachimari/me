71cf9f8f50ff4e18f1188b33060713edac06bd99
Ivan <opachimari@gmail.com>
2023-07-15 08:48:39 +0300

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/Avatar";
 import { Card, CardContent, CardFooter, CardHeader } from "./ui/Card";
 import Studlancer from "../public/notion.png";
+const projects: {
+  title: string;
+  preview: StaticImageData;
+  description: string;
+}[] = [
+  {
+    title: "Post everything with notion-like editor",
+    preview: Studlancer,
+    description: "Full-stack Next js 13 app with Dynamodb and real-time sync.",
+  },
+];
 
 export default function Page() {
   return (
@@ -17,30 +28,29 @@ export default function Page() {
         <p className="w-5/6 text-lg text-slate-600">{`Hi, I'm Thien (Ivan). I'm currently a business student. In my free time, I learn programming. `}</p>
         <p className="text-lg text-slate-600 my-10">{`Here are some of the projects that I made:`}</p>
         <div>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://studlancer-web.vercel.app"
-          >
-            <Card className="w-80">
-              <CardHeader>
-                <h3 className="font-bold">
-                  Post everything with notion-like editor
-                </h3>
-              </CardHeader>
-              <CardContent>
-                <Image
-                  src={Studlancer}
-                  alt="Project preview"
-                  width={500}
-                  height={500}
-                />
-              </CardContent>
-              <CardFooter>
-                Full-stack Next js 13 app with Dynamodb and real-time sync.{" "}
-              </CardFooter>
-            </Card>
-          </a>
+          {projects.map((p) => (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://studlancer-web.vercel.app"
+              key={p.title}
+            >
+              <Card className="w-80">
+                <CardHeader>
+                  <h3 className="font-bold">{p.title}</h3>
+                </CardHeader>
+                <CardContent>
+                  <Image
+                    src={Studlancer}
+                    alt="Project preview"
+                    width={500}
+                    height={500}
+                  />
+                </CardContent>
+                <CardFooter>{p.description}</CardFooter>
+              </Card>
+            </a>
+          ))}
         </div>
       </article>
     </section>
